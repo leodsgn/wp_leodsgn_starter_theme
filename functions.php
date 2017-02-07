d40443b7ed144bf856210f6d11811c0329d516d0
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2017-02-07 14:11:48 -0200

    Loaded scripts

--- functions.php
+++ functions.php
@@ -11,8 +11,6 @@
     /* 0. INCLUDES */
     /* --------------------------------------------------- */
 
-    include_once(get_template_directory() . '/include/frontend_functions/enqueue.php');
-
     /* --------------------------------------------------- */
     /* 1. DEFINE CONSTANTS  */
     /* --------------------------------------------------- */
@@ -25,10 +23,39 @@
     /* --------------------------------------------------- */
     /* 2. SETUP */
     /* --------------------------------------------------- */
+    if( !function_exists('enqueue_style_and_scripts') ) {
+        function enqueue_style_and_scripts() {
+            // register styles
+            wp_register_style( 'bootstrap_css', get_template_directory_uri() . '/public/lib/bootstrap/dist/css/bootstrap.min.css');
+            wp_register_style( 'main_css', get_template_directory_uri() . '/dist/styles/index.css');
+
+            // enqueue styles
+            wp_enqueue_style( 'bootstrap_css');
+            wp_enqueue_style( 'main_css');
+
+            // register scripts
+            wp_register_script( 'bootstrap_js', get_template_directory_uri() . '/public/lib/bootstrap/dist/js/bootstrap.min.js', array(), false, true);
+            wp_register_script( 'main_js', get_template_directory_uri() . '/dist/scripts/index.js', array('bootstrap_js'), 1, true);
+
+            // enqueue scripts
+            wp_enqueue_script( 'jquery');
+            wp_enqueue_script( 'bootstrap_js');
+            wp_enqueue_script( 'main_js');
+        }
+    }
+
+    add_theme_support( 'menus' );
+
+    if( !function_exists('leodsgn_setup_theme')) {
+        function leodsgn_setup_theme(){
+            register_nav_menu( 'primary', __('Primary Menu', 'leodsgn') );
+        }
+    }
 
     /* --------------------------------------------------- */
     /* 3. LOAD SCRIPTS AND STYLES */
     /* --------------------------------------------------- */
+    add_action( 'wp_enqueue_scripts', 'enqueue_style_and_scripts');
 
     /* --------------------------------------------------- */
     /* 4. ACTIONS AND FILTERS */
@@ -37,8 +64,6 @@
     /* --------------------------------------------------- */
     /* 5. DEFINE SHORTCODES */
     /* --------------------------------------------------- */
-
-
 
 
 ?>
--- include/frontend_functions/enqueue.php
+++ include/frontend_functions/enqueue.php
@@ -7,7 +7,7 @@
     /* 
     /* ---------------------------------------------------------------------------------------------- */  
 
-    function leodsgn_enqueue_styles(){
+    function leodsgn_enqueue(){
         
         // register styles
         wp_register_style( 'bootstrap_css', get_template_directory_uri() . '/public/lib/bootstrap/dist/css/bootstrap.min.css');
@@ -16,15 +16,13 @@
         // enqueue styles
         wp_enqueue_style( 'bootstrap_css');
         wp_enqueue_style( 'main_css');
-    }
 
-    function leodsgn_enqueue_scripts() {
-        
-        // register scripts
-        wp_register_script( 'bootstrap_js', get_template_directory_uri() . '/public/lib/bootstrap/dist/js/bootstrap.min.js');
-        wp_register_script( 'main_js', get_template_directory_uri() . '/dist/scripts/index.js');
+         // register scripts
+        wp_register_script( 'bootstrap_js', get_template_directory_uri() . '/public/lib/bootstrap/dist/js/bootstrap.min.js', array(), false, true);
+        wp_register_script( 'main_js', get_template_directory_uri() . '/dist/scripts/index.js', array('bootstrap_js'), 1, true);
 
         // enqueue scripts
+        wp_enqueue_script( 'jquery');
         wp_enqueue_script( 'bootstrap_js');
         wp_enqueue_script( 'main_js');
     }
